7939bc9c47d2d1dbe317856a506b6d5041d1f208
Mr. King <60671734+paul-e-king@users.noreply.github.com>
2024-09-19 12:01:53 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+// any import statements go here!
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code goes here!
+        System.out.println("Hello, world!");
+        // Yout code stops here!
+    }
+}
